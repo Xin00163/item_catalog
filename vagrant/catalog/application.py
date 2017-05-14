13e841837aacea7dfb0cee0d70d21e45e923f0cc
Xin00163 <wangxin00163@yahoo.co.uk>
2017-05-14 22:51:59 +0100

    add more templates

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Ingredient, RecipeItem
@@ -12,11 +12,25 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/')
+@app.route('/ingredients/<int:ingredient_id>/recipe/JSON')
+def ingredientRecipeJSON(ingredient_id):
+    ingredient = session.query(Ingredient).filter_by(id=ingredient_id).one()
+    items = session.query(RecipeItem).filter_by(ingredient_id=ingredient_id).all()
+    return jsonify(RecipeItem=[i.serialize for i in items])
+
+@app.route('/ingredients/<int:ingredient_id>/recipe/<int:recipe_id>/JSON/')
+def recipeItemJSON(ingredient_id, recipe_id):
+    Recipe_Item = session.query(RecipeItem).filter_by(id=recipe_id).one()
+    return jsonify(Recipe_Item=Recipe_Item.serialize)
+
+@app.route('/ingredient/JSON')
+def ingredientsJSON():
+    ingredients = session.query(Ingredient).all()
+    return jsonify(ingredients=[i.serialize for i in ingredients])
 
 # Show all ingredients
 @app.route('/')
-@app.route('/ingredient/')
+@app.route('/ingredients/')
 def showIngredients():
     ingredients = session.query(Ingredient).all()
     return render_template('ingredients.html', ingredients=ingredients)
@@ -24,7 +38,7 @@ def showIngredients():
 @app.route('/ingredient/new', methods=['GET', 'POST'])
 def newIngredient():
     if request.method == 'POST':
-        newIngredient = IngredientItem(name = request.form['name'])
+        newIngredient = Ingredient(name = request.form['name'])
         session.add(newIngredient)
         session.commit()
         flash("new ingredient item created!")
@@ -32,7 +46,7 @@ def newIngredient():
     else:
         return render_template('newIngredient.html')
 
-@app.route('/ingredient/<int:ingredient_id>/edit/', methods=['GET', 'POST'])
+@app.route('/ingredient/<int:ingredient_id>/edit', methods=['GET', 'POST'])
 def editIngredient(ingredient_id):
     editedIngredient = session.query(Ingredient).filter_by(id=ingredient_id).one()
     if request.method == 'POST':
@@ -43,8 +57,8 @@ def editIngredient(ingredient_id):
         return render_template(
         'editIngredient.html', ingredient=editedIngredient)
 
-@app.route('/ingredient/<int:ingredient_id>/delete/', methods=['GET', 'POST'])
-def deleteingredient(ingredient_id):
+@app.route('/ingredient/<int:ingredient_id>/delete', methods=['GET', 'POST'])
+def deleteIngredient(ingredient_id):
     ingredientToDelete = session.query(Ingredient).filter_by(id=ingredient_id).one()
     if request.method == 'POST':
         session.delete(ingredientToDelete)
@@ -55,7 +69,7 @@ def deleteingredient(ingredient_id):
             'deleteIngredient.html', ingredient=ingredientToDelete)
 
 @app.route('/ingredients/<int:ingredient_id>/')
-@app.route('/ingredients/<int:ingredient_id>/recipe/')
+@app.route('/ingredients/<int:ingredient_id>/recipe')
 def ingredientRecipe(ingredient_id):
     ingredient = session.query(Ingredient).filter_by(id=ingredient_id).one()
     print ingredient
@@ -63,7 +77,7 @@ def ingredientRecipe(ingredient_id):
     return render_template('recipe.html', ingredient=ingredient, items=items,
     ingredient_id=ingredient_id)
 
-@app.route('/ingredients/<int:ingredient_id>/new/', methods=['GET', 'POST'])
+@app.route('/ingredients/<int:ingredient_id>/recipe/new', methods=['GET', 'POST'])
 def newRecipeItem(ingredient_id):
     if request.method == 'POST':
         newItem = RecipeItem(name = request.form['name'], method=request.form[
@@ -76,20 +90,20 @@ def newRecipeItem(ingredient_id):
     else:
         return render_template('newrecipeitem.html', ingredient_id=ingredient_id)
 
-@app.route('/ingredients/<int:ingredient_id>/<int:recipe_id>/edit',
+@app.route('/ingredients/<int:ingredient_id>/recipe/<int:recipe_id>/edit',
             methods=['GET', 'POST'])
 def editRecipeItem(ingredient_id, recipe_id):
     editedItem = session.query(RecipeItem).filter_by(id=recipe_id).one()
     if request.method == 'POST':
         if request.form['name']:
-            edtiedItem.name = request.form['name']
+            editedItem.name = request.form['name']
         if request.form['method']:
             editedItem.method = request.form['method']
         if request.form['time_needed']:
             editedItem.time_needed = request.form['time_needed']
 
         session.add(editedItem)
-        flash("Menu Item has been edited")
+        flash("Recipe Item has been edited")
         session.commit()
         return redirect(url_for('ingredientRecipe', ingredient_id=ingredient_id))
     else:
@@ -103,7 +117,7 @@ def deleteRecipeItem(ingredient_id, recipe_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
-        flash("Menu Item has been deleted")
+        flash("Recipe Item has been deleted")
         return redirect(url_for('ingredientRecipe', ingredient_id=ingredient_id))
     else:
         return render_template('deleterecipe.html', item=itemToDelete)
